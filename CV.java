33db6ded0da170ff409e39d1bfd0a65291dfe85e
HetuPhilippe <philippehetu@yahoo.ca>
2019-01-28 13:59:16 -0500

    #2 Le constructeur CV

--- CV.java
+++ CV.java
@@ -1,9 +1,22 @@
 
 public class CV {
 
+	String nom = "", prenom = "",formation="",competences="",attentes="";
+	int nbanne=0;
+	
+	CV[] personnes = new CV[2];
 	public static void main( String[] args ) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	public CV(String nom, String prenom, String formation, String competences,String Attentes,int nbanne) {
+	this.nom = nom;
+	this.prenom = prenom;
+	this.formation = formation;
+	this.competences = competences;
+	this.attentes = Attentes;
+	this.nbanne = nbanne;
 	}
 
 }
