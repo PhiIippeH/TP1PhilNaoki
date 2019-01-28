1aea840eec2752941b6268fcc3313f44f83d3ee4
HetuPhilippe <philippehetu@yahoo.ca>
2019-01-28 14:35:56 -0500

    #3 Le main

--- CV.java
+++ CV.java
@@ -1,22 +1,29 @@
 
 public class CV {
 
-	String nom = "", prenom = "",formation="",competences="",attentes="";
-	int nbanne=0;
-	
-	CV[] personnes = new CV[2];
+	String nom = "", prenom = "", formation = "", competences = "", attentes = "";
+	int nbanne = 0;
+
 	public static void main( String[] args ) {
 		// TODO Auto-generated method stub
+		System.out.println( "Bienvenue chez Barette!" );
+		CV Naoki = new CV( "Miyazaki-Chapleau", "Naoki", "DEC Montmorency", "Ordinateurs",
+				"Comprendre et analyser en profondeur git", 2 );
+		affiche( Naoki );
+		CV Philippe = new CV( "Hétu", "Philippe", "DEC Montmorency", "Ordinateurs",
+				"Comprendre et analyser en profondeur git", 2 );
+		affiche( Philippe );
 
 	}
+
+	public CV( String nom, String prenom, String formation, String competences, String attentes, int nbanne ) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.formation = formation;
+		this.competences = competences;
+		this.attentes = attentes;
+		this.nbanne = nbanne;
+	}
+
 	
-	public CV(String nom, String prenom, String formation, String competences,String Attentes,int nbanne) {
-	this.nom = nom;
-	this.prenom = prenom;
-	this.formation = formation;
-	this.competences = competences;
-	this.attentes = Attentes;
-	this.nbanne = nbanne;
-	}
-
 }
