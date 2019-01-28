8a50daffb546f8b6b31561ac136021f421675456
Your Name <nuvmyt@gmail.com>
2019-01-28 13:59:14 -0500

    oups javais rien commit

--- CV.java
+++ CV.java
@@ -4,12 +4,19 @@ public class CV {
 
 	String nom="",prenom="",formation="",competences="",attentes="";
 	int nbannee=0;
-	public static void main(String[] args) {
+	CV[] personnes=new CV[2];
+	public void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Bienvenue chez Barette!");
+		personnes[0]=new CV("Miyazaki-Chapleau","Naoki","Euh ca veut dire quoi ca","");
 	}
-	public void cv() {
-		
+	public CV(String n, String p, String f, String c, String a, int ann) {
+		nom=n;
+		prenom=p;
+		formation=f;
+		competences=c;
+		attentes=a;
+		nbannee=ann;
 	}
 
 }
