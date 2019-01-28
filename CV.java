332717c9817bca090ff4b9dc04b3497564cb8b8d
Your Name <nuvmyt@gmail.com>
2019-01-28 13:36:07 -0500

    rajouter des affaires

--- CV.java
+++ CV.java
@@ -2,9 +2,14 @@ package assquallogtp1;
 
 public class CV {
 
+	String nom="",prenom="",formation="",competences="",attentes="";
+	int nbannee=0;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Bienvenue chez Barette!");
+	}
+	public void cv() {
+		
 	}
 
 }
